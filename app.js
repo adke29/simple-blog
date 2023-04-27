b678663b6c05b3f9c27a61bd1fad61070da04e58
Kevin Adrian <adrian.kevin@yahoo.com>
2023-04-28 00:52:52 +0800

    Update about-us, and contact me

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://admin-kevin:Test123456@cluster0.6ehnxuj.mongodb.net/blogDB");
 
 const homeStartingContent = "Welcome to Daily Journal, your trusted source for up-to-the-minute news and information. We pride ourselves on delivering the most relevant and insightful stories from around the world, covering everything from breaking news to the latest trends in politics, business, sports, entertainment, and more";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
@@ -39,7 +38,7 @@ app.get("/",async function(req,res){
 })
 
 app.get("/about",function(req,res){
-  res.render("about",{aboutMeContent:aboutContent})
+  res.render("about")
 })
 
 app.get("/contact",function(req,res){
